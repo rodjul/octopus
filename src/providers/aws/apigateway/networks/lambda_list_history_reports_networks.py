5c9bf827b542fa6d328ce3b911b833dce644bd60
Rodrigo Juliano <rodrigojuliano22@gmail.com>
2021-05-24 18:41:51 -0300

    fix(networks-reports): add sort asc

--- src/providers/aws/apigateway/networks/lambda_list_history_reports_networks.py
+++ src/providers/aws/apigateway/networks/lambda_list_history_reports_networks.py
@@ -21,7 +21,7 @@ def lambda_handler(event, context):
     history = list_history()
 
     if history:
-        history = sorted(history, key = lambda i : i['Timestamp'])
+        history.sort(key=lambda x: x['Timestamp'], reverse=True)
 
     # logs.write(event, "AWS",  200, "", "All dates available of Network Report was requested" )
     logs.write(event, "AWS", 200, event['body'], "Get dates of Network Report", "")
